ca79ad9e37cde5e5daa6b48d9cb423395e055802
Tatiana Vaganova <athdev@yandex.ru>
2024-10-24 00:30:12 +0300

    set up routes for the update and delete product resource endpoints

--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { createProduct, getProducts } from '../controllers/products';
+import {
+  createProduct,
+  getProducts,
+  deleteProduct,
+  updateProduct,
+} from '../controllers/products';
 import { validateProduct } from '../middlewares/validators';
 
 const router = Router();
 router.get('/', getProducts);
 router.post('/', validateProduct, createProduct);
+router.delete('/:productId', deleteProduct);
+router.patch('/:productId', updateProduct);
 
 export default router;
