15ecfc3843c71eb7a719c3ed97fb75c26b7b8f07
Tatiana Vaganova <athdev@yandex.ru>
2024-10-23 18:34:46 +0300

    feat/handle product requests via controller actions

---
+++ backend/src/controllers/products.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express';
+import Product from '../models/product';
+
+export const getProducts = async (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    const products = await Product.find({});
+    return res.send({ items: products, total: products.length });
+  } catch (error) {
+    if (error instanceof Error) {
+      next(`Ошибка при получении продуктов: ${error.message}`);
+    } else {
+      next('Произошла непредвиденная ошибка');
+    }
+  }
+};
+
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, image, category, description, price } = req.body;
+
+  try {
+    const isExist = await Product.findOne({ title });
+    if (isExist) {
+      return next(console.error(`Продукт с названием "${title}" уже существует`));
+    }
+
+    const product = await Product.create({
+      title,
+      image,
+      category,
+      description,
+      price,
+    });
+
+    if (!product) {
+      return next(console.error('Товар не найден после создания'));
+    }
+
+    return res.status(201).send({ data: product });
+  } catch (error) {
+    if (error instanceof Error) {
+      return next(error);
+    }
+    return next(console.error('Произошла непредвиденная ошибка'));
+  }
+};
